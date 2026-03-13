bdf34ce90d5536cf85933082bfd2041adb706c59
skonlabs <skonlabs@users.noreply.github.com>
2026-03-13 00:14:05 -0700

    chore: bump bugpilot to v0.3.0

--- Formula/bugpilot.rb
+++ Formula/bugpilot.rb
@@ -8,11 +8,11 @@ class Bugpilot < Formula
   on_macos do
     on_arm do
       url "https://github.com/skonlabs/bugpilot/releases/download/v#{version}/bugpilot-macos-arm64"
-      sha256 "edb38cf80e3500a54760a219792fbcc37c6f40f9216d23c68f5c16ffce956975"
+      sha256 "e53e09b307720844e2a1fb7a7857d2c315bdd1152d5adb70b546f56c168fd60e"
     end
     on_intel do
       url "https://github.com/skonlabs/bugpilot/releases/download/v#{version}/bugpilot-macos-x86_64"
-      sha256 "a43b0c18628aad3fce33f0bc2af63555616e12ffcd200d41dae2e448c69ff96a"
+      sha256 "6586f9a155b3197693c5cb3d8d5064b0212dc5bde3b8602833432ef1dbd463dc"
     end
   end
 
