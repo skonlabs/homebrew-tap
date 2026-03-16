936fd6ffdcd6dad50435120210a82cac86d99b33
skonlabs <skonlabs@users.noreply.github.com>
2026-03-16 05:12:42 -0700

    chore: bump bugpilot to v0.4.0

--- Formula/bugpilot.rb
+++ Formula/bugpilot.rb
@@ -1,43 +1,26 @@
-# Homebrew formula for the BugPilot CLI.
 class Bugpilot < Formula
   desc     "CLI-first developer tool for debugging production incidents"
   homepage "https://bugpilot.io"
-  version  "0.3.0"
+  version  "0.4.0"
   license  "Proprietary"
 
   on_macos do
     on_arm do
       url "https://github.com/skonlabs/bugpilot/releases/download/v#{version}/bugpilot-macos-arm64"
-      sha256 "78aa9f587f36f2c15bfa49043ca19764aa59c39a073dcf3cb752c15c38d9fccc"
+      sha256 "f244590aa3538479a47ef9a4a2ca44e1f87e7d92cfcb7cc82370318decf3fab8"
     end
     on_intel do
-      url "https://github.com/skonlabs/bugpilot/releases/download/v#{version}/bugpilot-macos-x86_64"
-      sha256 "3d1c4607f6ab1afa23541fdd92dd9dedf4a36a82e5550d72d78247a601bee91a"
+      url "https://github.com/skonlabs/bugpilot/releases/download/v#{version}/bugpilot-macos-amd64"
+      sha256 "0e7fec1d889c7ff3cfc554b993c6fdf4bbe404980fa274d5cae10b702929d30b"
     end
   end
 
   def install
     if Hardware::CPU.intel?
-      bin.install "bugpilot-macos-x86_64" => "bugpilot"
+      bin.install "bugpilot-macos-amd64" => "bugpilot"
     else
       bin.install "bugpilot-macos-arm64" => "bugpilot"
     end
-  end
-
-  def caveats
-    <<~EOS
-      Shell completions are not installed automatically.
-      To enable them, add one of the following to your shell profile:
-
-        # bash (~/.bash_profile or ~/.bashrc)
-        eval "\"
-
-        # zsh (~/.zshrc)
-        eval "\"
-
-        # fish (~/.config/fish/config.fish)
-        bugpilot --completion fish | source
-    EOS
   end
 
   test do
